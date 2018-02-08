7fd9474df4922ea2c15105c236d9b56aaf2d8352
Link <13788485679@sina.cn>
2018-02-08 17:34:48 +0800

    model部分修改为es6写法

--- src/model/actions/state.js
+++ src/model/actions/state.js
@@ -1,12 +1,23 @@
+// State 的变化，会导致 View 的变化。所以，State 的变化必须是 View 导致的。
+// Action 是一个对象，其中的 type 属性是必须的，表示 Action 的名称。其他属性可以自由设置，不过建议遵从 FSA 规范
+
 const ADD_TODO = 'ADD';
 
-let addTodo = function(id = 'id', num = 0){
+// let addTodo = function(id = 'id', num = 0){
+//   let json = {
+//     type: ADD_TODO,
+//     id: id,
+//     num: num
+//   }
+//   return json
+// };
+
+let addTodo = (id = 'id', num = 0) => {
   let json = {
     type: ADD_TODO,
     id: id,
     num: num
   }
   return json
-};
-
+}
 export default addTodo
--- src/model/reducers/state.js
+++ src/model/reducers/state.js
@@ -1,14 +1,17 @@
+// Store 收到 Action 以后，必须给出一个新的 State，这样 View 才会发生变化。
+// Reducer 是一个函数，它接受 Action 和当前 State 作为参数，返回一个新的 State。
+
 let defaultState = 0;
 const reducer = (state = defaultState, action) => {
   switch (action.type) {
     case 'ADD':
       console.log('dispatch ADD')
-      console.log(state)
-      let counting = state.count + action.num
-      return {
-        count: counting,
+      let counting = {
+        count: state.count + action.num,
         name: 'mainTree'
-      };
+      }
+      console.log(counting)
+      return counting;
     default:
       console.log('dispatch default')
       console.log(action.type)
